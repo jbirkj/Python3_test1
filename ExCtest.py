ec019c56c9ab57b59ff4daf905e2208def9baa35
jbirkj <jesperbirkp@gmail.com>
2015-01-02 22:11:15 +0100

    -Added temperature logging to google she using gspread. - gspread is install separately for Python and Python3 - path string is now written as two strings and not ...     made in loop by addding each integer

--- ExCtest.py
+++ ExCtest.py
@@ -1,5 +1,6 @@
 
-import time, sys
+import time, datetime, sys
+import gspread
 
 sys.path.append("/home/pi/quick2wire-python-api")
 
@@ -9,8 +10,7 @@ from quick2wire.i2c import I2CMaster, writing_bytes, reading
 address = 0x28
 Tekst = [ 0x54, 0x65, 0x6d, 0x70, 0x3d ]
 ROMCODE1 = [ 0x28, 0xc3, 0xc2, 0x9d, 0x04, 0x00, 0x00, 0x9b]
-iROMCODE1 = [ 40, 195, 194, 157, 4, 0, 0, 155]
-
+sRomCode1 = "40 195 194 157 4 0 0 155"
 
 def I2CWrite(value1) :
     I2CMaster().transaction( writing_bytes(address, value1) )
@@ -25,24 +25,30 @@ def I2CRead():
 
 print('starting...')
 
-path = "./DS18b20read.a "
-parse = path + repr(ROMCODE1)
+try:
+    gs = gspread.login('jesperbirkp@gmail.com', 'zxkfdgmtpslbqpzg')
+except:
+    print('fail')
+    sys.exit
 
-for a in range(0, 8 ):
-    path += str(iROMCODE1[a])
-    path += " "
+wks = gs.open("TempLog1").sheet1
 
-
+path = "./DS18b20read.a " + sRomCode1
 print(path)
-
+ 
 #proc = Popen(["sudo ./DS18b20read.a "],
 proc = Popen(path ,
              shell=True, 
             stdout=PIPE)
-
 stdoutvalue = proc.communicate()
+TempRead1 = eval(stdoutvalue[0])
+print("current temperature is ", TempRead1, "degree Celsius" )
 
-print("current temperature is ", eval(stdoutvalue[0]), "degree Celsius" )
+#writing to Google sheet
+values = [ datetime.datetime.now(), 'sensor1',
+           eval(stdoutvalue[0]), eval(stdoutvalue[0])+5 ]
+wks.append_row(values)
+
 
 # clear displa and move cursor to start
 I2CWrite(0xFE)
