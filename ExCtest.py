afd6387114a43056f74256811c8cd6c56add4d26
jbirkj <jesperbirkp@gmail.com>
2015-01-18 00:25:38 +0100

    no GUI, adding loop
    TKinter GUI removed
    added loop to measure and google upload temp at T

--- ExCtest.py
+++ ExCtest.py
@@ -15,6 +15,19 @@ ROMCODE1 = [ 0x28, 0xc3, 0xc2, 0x9d, 0x04, 0x00, 0x00, 0x9b]
 sRomCode1 = "40 195 194 157 4 0 0 155"
 ROMCODE5 = [ 0x28, 0xe8, 0xd4, 0x45, 0x05, 0x00, 0x00, 0x83]
 sRomCode5 = "40 232 212 69 5 0 0 131"
+ROMCODE2 = [ 0x28, 0x0c, 0x1b, 0xe0, 0x04, 0x00, 0x00, 0xb8]
+sRomCode2 = "40 12 27 224 4 0 0 184"
+
+#//1=build into metal, 2=2nd w short wiremount, 3=3rd w long wiremount, 4=loose 18B20 
+#//5=metal tip w silicone tube
+#cROMCODE1[0]=0x28;	cROMCODE2[0]=0x28;	cROMCODE3[0]=0x28;	cROMCODE4[0]=0x28;	cROMCODE5[0]=0x28;
+#cROMCODE1[1]=0xc3;	cROMCODE2[1]=0x0c;	cROMCODE3[1]=0xa6;	cROMCODE4[1]=0x2f;	cROMCODE5[1]=0xe8;
+#cROMCODE1[2]=0xc2;	cROMCODE2[2]=0x1b;	cROMCODE3[2]=0xd8;	cROMCODE4[2]=0x90;	cROMCODE5[2]=0xd4;
+#cROMCODE1[3]=0x9d;	cROMCODE2[3]=0xe0;	cROMCODE3[3]=0x9c;	cROMCODE4[3]=0x2d;	cROMCODE5[3]=0x45;
+#cROMCODE1[4]=0x04;	cROMCODE2[4]=0x04;	cROMCODE3[4]=0x04;	cROMCODE4[4]=0x04;	cROMCODE5[4]=0x05;
+#cROMCODE1[5]=0x00;	cROMCODE2[5]=0x00;	cROMCODE3[5]=0x00;	cROMCODE4[5]=0x00;	cROMCODE5[5]=0x0;
+#cROMCODE1[6]=0x00;	cROMCODE2[6]=0x00;	cROMCODE3[6]=0x00;	cROMCODE4[6]=0x00;	cROMCODE5[6]=0x0;
+#cROMCODE1[7]=0x9b;	cROMCODE2[7]=0xb8;	cROMCODE3[7]=0xfd;	cROMCODE4[7]=0xd9;	cROMCODE5[7]=0x83;
 
 def I2CWrite(value1) :
     I2CMaster().transaction( writing_bytes(address, value1) )
@@ -40,78 +53,57 @@ def GoogleSubmit(value1, value2):
     wks.append_row(values)
 
 print('starting...')    #printing in prompt
-root = Tk()
 
 path = "./DS18b20read.a "
 path1 = path + sRomCode1
-path2 = path + sRomCode5
-print(path1)
-print(path2)
+path2 = path + sRomCode2
+#print(path1)
+#print(path2)
+
+#Starting Loop
+
+Try:
+    While TRUE:
  
-#proc = Popen(["sudo ./DS18b20read.a "],
-proc = Popen(path1 ,
-             shell=True, 
-            stdout=PIPE)
-stdoutvalue = proc.communicate()
-TempRead1 = eval(stdoutvalue[0])
-print("current temperature 1 is ", TempRead1, "degree Celsius" )
+    #proc = Popen(["sudo ./DS18b20read.a "],
+    proc = Popen(path1 ,
+                 shell=True, 
+                stdout=PIPE)
+    stdoutvalue = proc.communicate()
+    TempRead1 = eval(stdoutvalue[0])
+    print("current temperature 1 is ", TempRead1, "degree Celsius" )
+    
+    proc = Popen(path2 ,
+                 shell=True, 
+                stdout=PIPE)
+    stdoutvalue = proc.communicate()
+    TempRead2 = eval(stdoutvalue[0])
+    print("current temperature 2 is ", TempRead2, "degree Celsius" )
+    
+    GoogleSubmit(TempRead1, TempRead2)
 
-proc = Popen(path2 ,
-             shell=True, 
-            stdout=PIPE)
-stdoutvalue = proc.communicate()
-TempRead2 = eval(stdoutvalue[0])
-print("current temperature 2 is ", TempRead2, "degree Celsius" )
+    # Write to LCD - clear displa and move cursor to start
+    I2CWrite(0xFE)
+    I2CWrite(0x58)
+    
+    # writing LCD line1 with temp1
+    for a in range(len(Tekst)):
+        I2CWrite(Tekst[a])
+    I2CWrite(0x30+(int(TempRead1/10)))      #I2CWrite(0x30+(int(eval(stdoutvalue[0])/10)))
+    I2CWrite(0x30+TempRead1%10)             #I2CWrite(0x30+eval(stdoutvalue[0])%10)
+    
+    # writing LCD line 2 with Temp2
+    for a in range(len(Tekst2)):
+        I2CWrite(Tekst2[a])
+    I2CWrite(0x30+(int(TempRead2/10)))
+    I2CWrite(0x30+TempRead2%10)
+    
+    time.sleep(10)
+    
+except KeyboardInterrupt:
+    print("program interruppted by keyboard!")
+    return
 
-#GoogleSubmit(TempRead1, TempRead2)
-
-#write to GUI
-topFrame = Frame(root)
-bottomFrame = Frame(root, width=300, height=300)
-topFrame.pack()
-bottomFrame.pack()
-
-label_1 = Label(topFrame, text="Temperatur 1")
-label_2 = Label(topFrame, text="Temperatur 2")
-label_3 = Label(topFrame, text=TempRead1)
-label_4 = Label(topFrame, text=TempRead2)
-entry_1 = Entry(topFrame)
-entry_2 = Entry(topFrame)
-label_5 = Label(topFrame, text="Logged at :")
-label_6 = Label(topFrame, text=datetime.datetime.now() )
-label_1.grid(row=0, sticky=E)
-label_2.grid(row=1, sticky=E)
-label_3.grid(row=0, column=1)
-label_4.grid(row=1, column=1)
-entry_1.grid(row=0, column=2)
-entry_2.grid(row=1, column=2)
-label_5.grid(row=2, column=0)
-label_6.grid(row=2, column=1)
-
-c = Checkbutton(topFrame, text="checkbox ")
-c.grid(row=3, columnspan=2)
-b = Button(topFrame, text="Submit to Google Sheet", command=GoogleSubmit(TempRead1,TempRead2))
-b.grid(row=4)
-
-t = Text(bottomFrame, height=10, width=30)
-t.pack()
-
-# Write to LCD - clear displa and move cursor to start
-I2CWrite(0xFE)
-I2CWrite(0x58)
-
-# writing LCD line1 with temp1
-for a in range(len(Tekst)):
-    I2CWrite(Tekst[a])
-I2CWrite(0x30+(int(TempRead1/10)))      #I2CWrite(0x30+(int(eval(stdoutvalue[0])/10)))
-I2CWrite(0x30+TempRead1%10)             #I2CWrite(0x30+eval(stdoutvalue[0])%10)
-
-# writing LCD line 2 with Temp2
-for a in range(len(Tekst2)):
-    I2CWrite(Tekst2[a])
-I2CWrite(0x30+(int(TempRead2/10)))
-I2CWrite(0x30+TempRead2%10)
 
 var=0
-root.mainloop()
 print( "Goodbye")
