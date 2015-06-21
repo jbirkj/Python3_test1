4a4df52a56cf363d25974fbdd3fc36f4cc6b3b46
jbirkj <jesperbirkp@gmail.com>
2015-06-21 23:43:07 +0200

    Update Logging.py
    added file logging functions. Init to create file and Log to store log data

--- Logging.py
+++ Logging.py
@@ -16,11 +16,17 @@ def GoogleSubmit(value1, value2, value3):
     #wks.close() 
 
 
-def textLog(T, V1, V2, V3):
-	logFileName = datetime.datetime.now().strftime("%y%m%d") + "FermentLogFile.csv"
-	fileH = open(	logFileName, 'a', newline='') #open with append option to add data to existing file
+def textLog(FN, T, V1, V2, V3):
+	fileH = open(	FN, 'a', newline='') #open with append option to add data to existing file
 	fObj = csv.writer(fileH)
 	fObj.writerow( [T, V1, V2, V3] )
 	del fObj
 	fileH.close()
+
+def textLogInit(fn):
+	filehandler = open(	FN , 'w', newline='') #option w creates new file for Writing
+	fileObject = csv.writer(filehandler)
+	fileObject.writerow( [datetime.datetime.now(), 0, 0, 0] )
+	del fileObject
+	filehandler.close()	
 	
